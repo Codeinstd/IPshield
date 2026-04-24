dddd85ef9a4a7d23a6895890446754ea4db758b1
codeinstd <designer.oladipupo@gmail.com>
2026-04-24 15:08:53 +0100

    fixed bug

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -12,10 +12,10 @@ module.exports = function authMiddleware(req, res, next) {
   const key = req.headers["x-api-key"] || req.query.apiKey;
 
   console.log("RECV:", JSON.stringify(key));
-  console.log("EXPC:", JSON.stringify(process.env.IPSHIELD_API_KEY));
-  console.log("MATCH:", key === process.env.IPSHIELD_API_KEY);
+  console.log("EXPC:", JSON.stringify(process.env.API_KEY));
+  console.log("MATCH:", key === process.env.API_KEY);
 
   if (!key) return res.status(401).json({ error: "Unauthorized", message: "Missing x-api-key header" });
-  if (key !== process.env.IPSHIELD_API_KEY) return res.status(403).json({ error: "Forbidden", message: "Invalid API key" });
+  if (key !== process.env.API_KEY) return res.status(403).json({ error: "Forbidden", message: "Invalid API key" });
   next();
 };
